9b460cc73dd1c172d3828a7cfbd61e6fc9137803
Immanuel Raj <immanuelr44@gmail.com>
2021-03-22 21:10:14 +0530

    Add script for ota

---
+++ ota.sh
@@ -0,0 +1,22 @@
+DEVICE=$1
+FLAVOUR=$2
+BRAND=$3
+MODEL=$4
+DEV=$5
+
+if [ $# -lt 5 ]; then
+    echo "Missing mandatory parameters it must be like this :- bash ota.sh jasmine_sprout vanilla Xioami A2 "IMMANUEL""
+    exit 1
+fi
+
+DATETIME=$(grep "ro.build.date.utc=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
+FILENAME=$(find out/target/product/$DEVICE/LegionOS-v*.zip | cut -d "/" -f 5)
+ID=$(sha256sum out/target/product/$DEVICE/LegionOS-v*.zip | cut -d " " -f 1)
+FILEHASH=$ID
+SIZE=$(wc -c out/target/product/$DEVICE/LegionOS-v*.zip | awk '{print $1}')
+URL="https://sourceforge.net/projects/legionrom/files/$DEVICE/$FILENAME/download"
+VERSION="v11.0"
+ROMTYPE="OFFICIAL"
+JSON_FMT='{ "response": [ { "datetime": %s,"filename": %s,"id":%s,"romtype": "%s", "size":%s, "url":"%s", "version": "%s","device_brand": "%s","device_model": "%s","device_codename": "%s","developer": "%s"} ] }'
+printf "$JSON_FMT" "$DATETIME" "$FILENAME" "$ID" "$ROMTYPE" "$SIZE" "$URL" "$VERSION" "$BRAND" "$MODEL" "$DEVICE" "$DEV"> OTA/$DEVICE/$FLAVOUR.json
+echo $/OTA/$DEVICE/$FLAVOUR.json file created
